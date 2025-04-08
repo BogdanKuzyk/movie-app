4f3f7a61d6c5df8f3a53d127d2abbf1ef2405f94
Kuzyk Bogdan <bogdankuzyk14@gmail.com>
2025-04-08 21:07:32 +0300

    Add Hero Title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <h1>
+          Find <span className="text-gradient">Movies</span> You'll Enjoy
+          wihtout the Hassle
+        </h1>
+      </div>
+    </main>
+  );
 }
 
 export default App;
