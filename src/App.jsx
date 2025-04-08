9a38a4acefcf6b810a80d163c23cfb7c9c5cb45e
Kuzyk Bogdan <bogdankuzyk14@gmail.com>
2025-04-09 02:48:18 +0300

    Create movie card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -77,9 +78,7 @@ function App() {
           ) : (
             <ul>
               {movies.map((movie) => (
-                <li key={movie.id} className="text-white">
-                  {movie.title}
-                </li>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
